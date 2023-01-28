1bba7f6fd7288849d77ff18002c21e5a95501540
Jakestep <jakestep.17@gmail.com>
2023-01-27 20:51:44 -0700

    .

---
+++ Blackjack/index.js
@@ -0,0 +1,13 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+// Render the welcome message using welcomeEl.innerText
+
+
+let welcomeEl = document.getElementById("welcome-el")
+let greeting = "Hello, welcome to my website, "
+let name = "jake"
+
+welcomeEl.innerText = greeting + name
